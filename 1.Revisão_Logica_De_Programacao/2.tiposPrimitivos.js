80baa5786240a1006b647ac67256110a6b994a10
Emerson Oliveira <emerson.520@outlook.com>
2024-02-11 03:23:00 -0300

    Tipos Primitivos

---
+++ 1.Revisão_Logica_De_Programacao/2.tiposPrimitivos.js
@@ -0,0 +1,22 @@
+//strings, number (int, floats), boolean
+
+let minhaVar = "Olá, 'Mundo!'";
+let minhaVar2 = 'Exemplo "string"';
+
+console.log(minhaVar);
+console.log(minhaVar2);
+
+let idade = 38;
+let testeTemplateLiterals = `Minha templete literals`;
+
+console.log(testeTemplateLiterals);
+
+console.log(`Eu tenho ${idade} anos`);
+console.log("Eu tenho " + idade + " anos");
+
+console.log(typeof idade, typeof minhaVar);
+
+const numero1 = 10;
+const numero2 = 10.1;
+console.log(`O tipo de numero1 é ${typeof numero1} e seu valor é ${numero1}`);
+console.log(`O tipo de numero2 é ${typeof numero2} e seu valor é ${numero2}`);
