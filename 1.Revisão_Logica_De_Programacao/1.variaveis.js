cb5d40da4a25ed9ceda11250347a151b4bcee0ac
Emerson Oliveira <emerson.520@outlook.com>
2024-02-11 02:40:09 -0300

    Criando variáveis

---
+++ 1.Revisão_Logica_De_Programacao/1.variaveis.js
@@ -0,0 +1,19 @@
+//var let const
+
+let testeString = "Minha string";
+
+console.log(testeString);
+
+testeString = 10;
+
+console.log(testeString);
+
+const numero = 12;
+
+console.log(numero);
+
+let teste;
+
+teste = 'M';
+
+console.log(teste);
